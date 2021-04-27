aa4af899bd6ec8e46526c875c60bf09e68c82108
Roberto Calzadilla <82472131+Roberto121c@users.noreply.github.com>
2021-04-27 18:14:56 -0500

    Update Date_Query_P1.sql

--- Query/Date_Query_P1.sql
+++ Query/Date_Query_P1.sql
@@ -4,23 +4,12 @@
 SELECT 
       [DateKey],
       [FullDateAlternateKey] as Date,
-      --,[DayNumberOfWeek]
       [EnglishDayNameOfWeek] as Day,
-      --,[SpanishDayNameOfWeek]
-      --,[FrenchDayNameOfWeek]
-      --,[DayNumberOfMonth]
-      --,[DayNumberOfYear]
       [WeekNumberOfYear] as WeekNo,
       [EnglishMonthName] as Month,
-      --,[SpanishMonthName]
-      --,[FrenchMonthName]
       [MonthNumberOfYear] as MonthNo,
       [CalendarQuarter] as Quarter,
       [CalendarYear] as Year
-      --,[CalendarSemester]
-      --,[FiscalQuarter]
-      --,[FiscalYear]
-      --,[FiscalSemester]
 FROM 
       [AdventureWorksDW2019].[dbo].[DimDate]
 WHERE 
