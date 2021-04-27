4ca7d2981b77e40ebd86793b331c164bd8301779
Roberto Calzadilla <82472131+Roberto121c@users.noreply.github.com>
2021-04-26 20:59:11 -0500

    comment elimination

--- Customer_Query_P1.sql
+++ Customer_Query_P1.sql
@@ -2,39 +2,15 @@
 -- Limpieza de tabla de clientes
 SELECT  
        c.customerKey as Customer_Key,
-      --,[GeographyKey]
-      --,[CustomerAlternateKey]
-      --,[Title]
        c.FirstName as First_name,
-      --,[MiddleName]
        c.LastName as Last_name,
 	   c.FirstName + ' ' + c.LastName as Fullname,
-      --,[NameStyle]
-      --,[BirthDate]
-      --,[MaritalStatus]
-      --,[Suffix]
 CASE
        c.Gender WHEN 'M' THEN 'Male'
 	            WHEN 'F' THEN 'Female'
 				END 
 				AS Gender,
-      --,[EmailAddress]
-      --,[YearlyIncome]
-      --,[TotalChildren]
-      --,[NumberChildrenAtHome]
-      --,[EnglishEducation]
-      --,[SpanishEducation]
-      --,[FrenchEducation]
-      --,[EnglishOccupation]
-      --,[SpanishOccupation]
-      --,[FrenchOccupation]
-      --,[HouseOwnerFlag]
-      --,[NumberCarsOwned]
-      --,[AddressLine1]
-      --,[AddressLine2]
-      --,[Phone]
        c.DateFirstPurchase as Date_First_Purchase,
-      --,[CommuteDistance]
 	   g.city as Customer_city  -- Union desde la tabla Geography 
 FROM 
        [AdventureWorksDW2019].[dbo].[DimCustomer] as c
